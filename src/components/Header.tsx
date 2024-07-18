5ac24b980ece0f33b3db21f1ace2cca34e5ecc06
Fer <ferp@protonmail.ch>
2024-07-18 19:18:25 +0200

    first working session sign in and out

---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+"use client"
+import { signOut, signIn } from "next-auth/react";
+import { Session } from "next-auth";
+
+type HeaderProps = {
+  sessionData: Session | null;
+};
+
+export const Header = ({ sessionData }: HeaderProps) => {
+  // const { data: sessionData } = useSession();
+
+  return (
+    <div className="navbar bg-primary text-primary-content">
+      <div className="flex-1 pl-5 text-3xl font-bold">
+        {sessionData?.user?.name ? `Notes for ${sessionData.user.name}` : ""}
+        test
+      </div>
+      <div className="dropdown dropdown-end">
+        { sessionData?.user ?(
+        <label
+          tabIndex={0}
+          className="btn-ghost btn-circle avatar btn"
+          onClick={() => void signOut()}
+        >
+          <div className="w-10 rounded-full">
+            <img
+              src={sessionData?.user?.image ?? ""}
+              alt={sessionData?.user?.name ?? ""}
+            />
+          </div>
+        </label>
+
+        ) : (
+          <button
+            className="btn-ghost rounded-btn btn"
+            onClick={() => void signIn()}
+            >
+              Sign in
+            </button>
+        )
+        }
+      </div>
+    </div>
+  );
+};
