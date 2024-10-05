aa28890983fd063d3fc868cca304ff3f1b0acb2e
Fer <ferp@protonmail.ch>
2024-10-05 18:15:03 +0200

    clarify add topic interface

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -5,6 +5,7 @@ import type { Session } from "next-auth";
 import { api } from "~/trpc/react";
 import { NoteEditor } from "./NoteEditor";
 import { NoteCard } from "./NoteCard";
+import { set } from "zod";
 
 type Props = {
   sessionData: Session | null;
@@ -12,6 +13,7 @@ type Props = {
 
 export const Content = ({ sessionData }: Props) => {
   const [selectedTopic, setSelectedTopic] = useState<Topic | null>(null);
+  const [newTopic, setNewTopic] = useState<string>("");
   const { data: topics, refetch: refetchTopics } = api.topic.getAll.useQuery(
     undefined, // no input
     {
@@ -28,22 +30,22 @@ export const Content = ({ sessionData }: Props) => {
   const { data: notes, refetch: refetchNotes } = api.note.getAll.useQuery(
     { topicId: selectedTopic?.id ?? "" },
     {
-      enabled: sessionData?.user !== undefined && selectedTopic !== null
-    }
-  )
+      enabled: sessionData?.user !== undefined && selectedTopic !== null,
+    },
+  );
 
   const createNote = api.note.create.useMutation({
     onSuccess: () => {
       void refetchNotes();
-    }
-  })
+    },
+  });
 
   const deleteNote = api.note.delete.useMutation({
-    onSuccess: () => void refetchNotes()
-  })
+    onSuccess: () => void refetchNotes(),
+  });
 
   useEffect(() => {
-    setSelectedTopic( selectedTopic => selectedTopic ?? topics?.[0] ?? null);
+    setSelectedTopic((selectedTopic) => selectedTopic ?? topics?.[0] ?? null);
   }, [topics]);
 
   return sessionData?.user ? (
@@ -51,8 +53,9 @@ export const Content = ({ sessionData }: Props) => {
       <div className="px-2">
         <ul className="menu w-56 rounded-box bg-base-100 p-2">
           {topics?.map((topic) => (
-            <li key={topic.id}>
+            <li key={topic.id} className="pb-2">
               <a
+                className={`menu-item ${topic.id === selectedTopic?.id ? "bg-primary-content" : ""}`}
                 href="#"
                 onClick={(e) => {
                   e.preventDefault();
@@ -60,44 +63,47 @@ export const Content = ({ sessionData }: Props) => {
                 }}
               >
                 {topic.title}
-                {topic.id === selectedTopic?.id ? "selected" : ""}
               </a>
             </li>
           ))}
         </ul>
         <div className="divider"></div>
-        <input
-          type="text"
-          placeholder="New topic"
-          className="input input-sm input-bordered w-full"
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              createTopic.mutate({
-                title: e.currentTarget.value,
-              });
-              e.currentTarget.value = "";
-            }
-          }}
-        />
+        <form className="flex flex-col" onSubmit={e => {
+          e.preventDefault();
+          createTopic.mutate({
+            title: newTopic,
+          });
+          setNewTopic("");
+        }}>
+          <input
+            type="text"
+            placeholder="New topic"
+            className="input input-sm input-bordered w-full"
+            onChange={(e) => setNewTopic(e.currentTarget.value)}
+          />
+          <button className="btn btn-primary btn-sm mt-2 self-end">
+            Add topic
+          </button>
+        </form>
       </div>
       <div className="col-span-3">
         <div>
-          {notes?.map( (note) => (
+          {notes?.map((note) => (
             <div key={note.id}>
               <NoteCard
                 note={note}
-                onDelete={() => void deleteNote.mutate({ id: note.id})}
-                />
+                onDelete={() => void deleteNote.mutate({ id: note.id })}
+              />
             </div>
           ))}
         </div>
-        <NoteEditor 
-          onSave={({title, content}) => {
+        <NoteEditor
+          onSave={({ title, content }) => {
             void createNote.mutate({
               title,
               content,
-              topicId: selectedTopic?.id ?? ""
-            })
+              topicId: selectedTopic?.id ?? "",
+            });
           }}
         />
       </div>
